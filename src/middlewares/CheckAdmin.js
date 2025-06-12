766cb791e94a460d4f56aa457fa97589ddf47a35
Abbas2003 <abbas.mohammad805@gmail.com>
2025-06-13 04:08:37 +0500

    Refactor VerifyAdmin middleware to use constants for user types and improve role check logic

--- src/middlewares/CheckAdmin.js
+++ src/middlewares/CheckAdmin.js
@@ -1,14 +1,18 @@
 
 
+const USER_TYPE_SUPER_ADMIN = "super admin";
+const USER_TYPE_ADMIN = "admin";
+
 export const VerifyAdmin = (req, res, next) => {
     try {
         const user = req.user;
+
         if (!user) {
             return res.status(401).json({ message: "Unauthorized access. No user found." });
         }
 
-        // Check if the user is a super admin
-        if (user.user_type !== "super admin" || user?.user_type !== "admin") {
+        // Check if the user is a super admin or admin
+        if (user.user_type !== USER_TYPE_SUPER_ADMIN && user?.user_type !== USER_TYPE_ADMIN) {
             return res.status(403).json({ message: "Forbidden. You do not have Super Admin/Admin access." });
         }
 
